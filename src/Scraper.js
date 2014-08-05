cea9ece73deced7ed68b60a972ef660d6b9df8fd
Rui Gil <rui.web@gmail.com>
2014-08-05 15:28:30 +0100

    Added bower_components to .gitignore. Added phantom to the dependencies. Added some information to package.json. Fixed createStatic and createDynamic methods, from Scraper.js

--- src/Scraper.js
+++ src/Scraper.js
@@ -1,29 +1,26 @@
-var SimpleScraper = require('./StaticScraper.js'),
+var StaticScraper = require('./StaticScraper.js'),
 	DynamicScraper = require('./DynamicScraper.js'),
 	ScraperPromise = require('./ScraperPromise.js');
 
-function initScraper(type, url) {
-	var promise = new ScraperPromise();
+function initScraper(ScraperType, url) {
+	var promise = new ScraperPromise(),
+		scraper = new ScraperType(function(err, scraper) {
+			promise._fire(err, scraper);
+		});
 	if (url) {
-		type(url, function(error, scraper) {
-			promise._fire(error, scraper);
-		});
-	} else {
-		type(function(error, scraper) {
-			promise._fire(error, scraper);
-		});
+		scraper.get(url);
 	}
 	return promise;
 }
 
 module.exports = {
 	createStatic: function createStatic(url) {
-		return initScraper(SimpleScraper, url);
+		return initScraper(StaticScraper, url);
 	},
 	createDynamic: function createDynamic(url) {
-		return initScraper(DynamicScraper, url);
+		return initScraper(StaticScraper, url);
 	},
-	SimpleScraper: SimpleScraper,
+	StaticScraper: StaticScraper,
 	DynamicScraper: DynamicScraper,
 	ScraperPromise: ScraperPromise
 };
