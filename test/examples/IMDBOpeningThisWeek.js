b6225b9404aa4b83dc82a4ccb0447b0d3097f463
Rui Gil <rui.web@gmail.com>
2014-08-23 20:54:46 +0100

    Added IMDB's opening this week example.

---
+++ test/examples/IMDBOpeningThisWeek.js
@@ -0,0 +1,20 @@
+/* global $ */
+var sjs = require('scraperjs');
+/**
+ * Displays the movies opening this week, from IMDB.
+ * This example is inspired by user jasode at Hacker News.
+ *   {@link https://news.ycombinator.com/item?id=8193522}
+ *   Note that the list of movies opening this week is loaded
+ *   dynamically. A static scraper can't scrape this content, this way.
+ */
+sjs.DynamicScraper
+	.create('https://www.imdb.com')
+	.scrape(function() {
+		return $('.otw-title').map(function() {
+			return $(this).text().trim();
+		}).get();
+	}, function(movies) {
+		movies.forEach(function(movie) {
+			console.log(movie);
+		});
+	});
